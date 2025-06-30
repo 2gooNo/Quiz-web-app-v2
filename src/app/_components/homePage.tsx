d93716c3d0096f1e21fbb3654fa0a940ecf919b9
2gooNo <143684446+2gooNo@users.noreply.github.com>
2025-06-30 18:16:55 +0800

    idk

--- src/app/_components/homePage.tsx
+++ src/app/_components/homePage.tsx
@@ -1,11 +1,22 @@
 export function HomePage() {
   return (
-    <div>
-      <div>
-        <h1 className="font-bold text-3xl w-10">
-          Learn new concepts for each question
+    <div className="p-20">
+      <div className="flex gap-10 flex-col">
+        <h1 className="font-semibold text-5xl w-150 leading-17">
+          Learn<br/> new concepts<br/> for each question
         </h1>
+        <div className="border-solid border-l-2 border-l-[#333333] pl-8 text-[#828282]">We help you prepare for exams and quizzes </div>
+        <div>
+          <button>
+            Start solving
+          </button>
+          <button className="flex ">
+            <img src="/dropDownIcon.png" alt="Dropdown Icon"/>
+            <p>know more</p>
+          </button>
+        </div>
       </div>
     </div>
   );
 }
+
